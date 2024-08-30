eefa9b5ae383df4757a9afe7f71760f891c8b188
uwi0 <kakapoweb@gmail.com>
2024-08-30 13:22:18 +0700

    Initial Commit

---
+++ Complex Text Field/Complex_Text_FieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Complex_Text_FieldApp.swift
+//  Complex Text Field
+//
+//  Created by dwi prasetyo on 30/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Complex_Text_FieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
